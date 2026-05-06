99449f18c10003279e1c4709c15249b3e2d8c703
andreycadimazup <andrey.dias@zup.com.br>
2026-05-06 16:40:09 -0300

    chore: improvements

--- src/ops_setup/__init__.py
+++ src/ops_setup/__init__.py
@@ -42,6 +42,52 @@ def create_agent_files(repo: Repository, setup_branch: str):
             branch=setup_branch
         )
 
+def create_agentic_files(repo: Repository, setup_branch: str):
+    questionary.print("Adding Agentic skill files...")
+
+    create_file(
+        repo,
+        path=".agentic/SQUAD.md",
+        message=f"feat(ops): add agentic SQUAD",
+        content=load_file_content(f"./src/ops_setup/assets/agentic/SQUAD.md"),
+        branch=setup_branch
+    )
+
+    create_file(
+        repo,
+        path=".agentic/GUARDRAILS.md",
+        message=f"feat(ops): add agentic GUARDRAILS",
+        content=load_file_content(f"./src/ops_setup/assets/agentic/GUARDRAILS.md"),
+        branch=setup_branch
+    )
+
+    agent_skills = [
+       "architect-skill",
+       "developer-skill",
+       "orchestrator-skill",
+       "product-manager-skill",
+       "reviewer-skill"
+    ]
+
+    for skill in agent_skills:
+        asset_path = f"./src/ops_setup/assets/agentic/skills/{skill}/SKILL.md"
+        create_file(
+            repo,
+            path=f".agentic/skills/{skill}/SKILL.md",
+            message = f"feat(ops): add agentic {skill.replace('-', ' ')}",
+            content=load_file_content(asset_path),
+            branch=setup_branch
+        )
+
+        if skill == "orchestrator-skill":
+            create_file(
+                repo,
+                path=f".agentic/skills/{skill}/references/context-hooks.md",
+                message=f"feat(ops): add agentic {skill.replace('-', ' ')} context hooks ref",
+                content=load_file_content(f"./src/ops_setup/assets/agentic/skills/{skill}/references/context-hooks.md"),
+                branch=setup_branch
+            )
+
 
 def create_github_workflow_files(repo: Repository, setup_branch: str, selected_provider: str):
     questionary.print("Adding GitHub workflow: agentic-squad.yml...")
@@ -74,7 +120,8 @@ def create_ops_pr(repo: Repository, setup_branch: str, selected_provider: str, b
 Adding the necessary files for One Person Squad to function with the provider: **{selected_provider}**
 
 ## Changes
-- [x] Added Agentic Squad Github workflow
+- [x] Added Agent Squad Github workflow
+- [x] Added Agent Skill files
 - [x] Added Agentic Skill files
 """
     repo.create_pull(
@@ -99,6 +146,8 @@ def main() -> None:
     prepare_ops_setup_branch(repo, setup_branch)
 
     create_github_workflow_files(repo, setup_branch, selected_provider)
+
+    create_agentic_files(repo, setup_branch)
 
     create_agent_files(repo, setup_branch)
 
